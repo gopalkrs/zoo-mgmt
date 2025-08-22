75fa88ecf558aacae3df55d9d341fb77f7b376a9
Gaurav-Janjvadiya <janjvadiyagaurav344@gmail.com>
2025-08-22 22:28:35 +0530

    Implement CRUD operations for animals; add error handling and animal model

---
+++ src/app/api/animals/route.ts
@@ -0,0 +1,39 @@
+import { AnimalService } from "@/app/services";
+import { Animal } from "@/types/Animal";
+import { handleError } from "@/utils";
+
+/**
+ * POST /api/animals
+ * Create a new animal entry.
+ *
+ * @param {Request} req - The incoming request containing animal data in JSON body
+ * @returns {Promise<Response>} JSON response with success flag and created animal
+ */
+export async function POST(req: Request): Promise<Response> {
+  try {
+    const body = await req.json();
+
+    // Calls service layer to create new animal
+    const result = await AnimalService.createAnimal(body);
+
+    return Response.json({ success: true, result });
+  } catch (err: unknown) {
+    return handleError(err);
+  }
+}
+
+/**
+ * GET /api/animals
+ * Fetch all animals.
+ *
+ * @returns {Promise<Response>} JSON response with success flag and list of animals
+ */
+export async function GET(): Promise<Response> {
+  try {
+    const result: Animal[] = await AnimalService.getAllAnimals();
+
+    return Response.json({ success: true, result });
+  } catch (err: unknown) {
+    return handleError(err);
+  }
+}
