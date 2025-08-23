9347fd9a2c0417d19761e2f19e5954c8c34474fc
gopalkrs <raunakchd1@gmail.com>
2025-08-23 18:03:40 +0530

    feat: implement ticket booking and retrieval logic with TicketService

--- src/app/api/tickets/route.ts
+++ src/app/api/tickets/route.ts
@@ -1,16 +1,58 @@
 import { connectDB } from "@/lib/db";
-import { TicketService } from "@/services/ticketService";
+import { TicketService } from "@/app/services/ticket.services";
 import { NextRequest } from "next/server";
 
 const ticketService = new TicketService();
 
 export async function POST(req: NextRequest) {
-  await connectDB();
-  const ticketData = await req.json();
-  ticketService.bookTicket(ticketData);
+  try {
+    await connectDB();
+    const ticketData = await req.json();
+    if (new Date(ticketData.dateOfVisit).getTime() < new Date().getTime()) {
+      return Response.json(
+        { error: "Date of visiting cannot be in past" },
+        { status: 400 }
+      );
+    }
+    if (new Date(ticketData.dateOfVisit).getTime() > new Date().getTime()) {
+      const ticketListForDate = await ticketService.getTicketsByVisitDate(
+        new Date(ticketData.dateOfVisit)
+      );
+      if (ticketListForDate.length >= 100) {
+        return Response.json(
+          { error: "Pre Booking limit has been reached for this date" },
+          { status: 400 }
+        );
+      } else {
+        const ticketResult = await ticketService.bookTicket(ticketData);
+        //console.log(ticketResult);
+        return Response.json(
+          { message: "Pre Booking Successfull", ticket: ticketResult },
+          { status: 201 }
+        );
+      }
+    }
+    const ticketResult = await ticketService.bookTicket(ticketData);
+
+    return Response.json(
+      { message: "Ticket Booked Successfully", ticket: ticketResult },
+      { status: 201 }
+    );
+  } catch (error: unknown) {
+    console.error("Error creating ticket:", error);
+    return Response.json({ error: "Failed to book ticket" }, { status: 500 });
+  }
 }
 
 export async function GET() {
   await connectDB();
-  ticketService.getTickets();
+  try {
+    const ticketsList = await ticketService.getTickets();
+    return Response.json(
+      { message: "Tickets fetched successfully", result: ticketsList },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error finding tickets:", error);
+  }
 }
---
+++ src/app/services/ticket.services.ts
@@ -0,0 +1,36 @@
+import ticketModel from "@/models/ticket.model";
+import { TicketDocument, TicketInput } from "@/types/ticket";
+
+export class TicketService {
+  /**
+   * Create a new ticket entry in the database.
+   * @param {TicketInput} data - Ticket object containing the required fields
+   * @returns {Promise<TicketDocument>} The created ticket document
+   */
+  async bookTicket(ticketData: TicketInput): Promise<TicketDocument> {
+    const result = await ticketModel.create(ticketData);
+    console.log("[bookTicket] result:", result);
+    return result;
+  }
+
+  /**
+   * Fetch all tickets from the database.
+   * @returns {Promise<TicketDocument []>} Array of all ticket booked
+   */
+  async getTickets(): Promise<TicketDocument[]> {
+    const tickets = await ticketModel.find();
+    return tickets;
+  }
+
+  /**
+   * Fetch all tickets from the database for the given date.
+   ** @param {Date} data - date of visit
+   * @returns {Promise<TicketDocument []>} Array of all ticket booked on the given date
+   */
+  async getTicketsByVisitDate(dateOfVisit: Date): Promise<TicketDocument[]> {
+    const ticketListForDate = await ticketModel.find({
+      dateOfVisit: dateOfVisit,
+    });
+    return ticketListForDate;
+  }
+}
